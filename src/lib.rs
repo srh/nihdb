14cd17dbdadc7f3fbe76b31de31063dd476260a2
Sam Hughes <sam@samuelhughes.com>
2017-12-29 18:29:13 -0800

    Basic bogus interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,32 @@
+mod rihdb {
+    pub struct Store {
+
+    }
+
+    impl Store {
+        pub fn new() -> Store {
+            return Store{};
+        }
+
+        pub fn put(&mut self, key: &str, val: &str) {
+            println!("put '{}', '{}'", key, val);
+        }
+
+        pub fn get(&mut self, key: &str) -> String {
+            return "Hey".to_string();
+        }
+    }
+
+}
+
 #[cfg(test)]
 mod tests {
+    use rihdb::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn putget() {
+        let mut kv = Store::new();
+        kv.put("foo", "Hey");
+        let x: String = kv.get("foo");
+        assert_eq!("Hey", x);
     }
 }
