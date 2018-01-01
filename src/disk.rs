1d1c546dc9945cf4df7ba60bd9c6ddad0fcbc7cc
Sam Hughes <sam@samuelhughes.com>
2018-01-01 11:58:35 -0800

    TableBuilder

--- src/disk.rs
+++ src/disk.rs
@@ -59,35 +59,61 @@ fn decode_mutation(v: &[u8], pos: &mut usize) -> Option<Mutation> {
     }
 }
 
-// Returns keys_offset, file_size, smallest key, biggest key.
-pub fn flush_to_disk(dir: &str, table_number: u64, m: &MemStore) -> Result<(u64, u64, String, String)> {
-    assert!(!m.entries.is_empty());
-    let mut value_buf: Vec<u8> = Vec::new();
-    let mut key_buf: Vec<u8> = Vec::new();
-    
-    let mut first_key: Option<&str> = None;
-    let mut last_key: Option<&str> = None;
-    for (key, value) in m.entries.iter() {
-        last_key = Some(key);
-        if first_key.is_none() {
-            first_key = last_key;
-        }
-        encode_uint(&mut key_buf, value_buf.len() as u64);
-        encode_str(&mut key_buf, key);
-        encode_mutation(&mut value_buf, value);
+struct TableBuilder<'a> {
+    values_buf: Vec<u8>,
+    keys_buf: Vec<u8>,
+    first_key: Option<&'a str>,
+    last_key: Option<&'a str>,
+}
+
+impl<'a> TableBuilder<'a> {
+    fn new() -> TableBuilder<'a> {
+        return TableBuilder{
+            values_buf: Vec::new(),
+            keys_buf: Vec::new(),
+            first_key: None,
+            last_key: None,
+        };
     }
-    let keys_offset = value_buf.len() as u64;
-    encode_u64(&mut key_buf, keys_offset);  // NOTE: Not necessary if in TOC.
+
+    // This method has to be called in increasing order.
+    fn add_mutation(&mut self, key: &'a str, value: &Mutation) {
+        self.last_key = Some(key);
+        if self.first_key.is_none() {
+            self.first_key = self.last_key;
+        }
+        encode_uint(&mut self.keys_buf, self.values_buf.len() as u64);
+        encode_str(&mut self.keys_buf, key);
+        encode_mutation(&mut self.values_buf, value);
+    }
+
+    // Returns keys_offset, file_size, smallest key, biggest key.
+    fn finish(&mut self, writer: &mut Write) -> Result<(u64, u64, String, String)> {
+        assert!(!self.first_key.is_none());
+        let keys_offset = self.values_buf.len() as u64;
+        encode_u64(&mut self.keys_buf, keys_offset);  // NOTE: Not necessary now that it's in TOC.
+        writer.write_all(&self.values_buf)?;
+        writer.write_all(&self.keys_buf)?;
+        writer.flush()?;
+        return Ok((
+            keys_offset,
+            keys_offset + self.keys_buf.len() as u64,
+            self.first_key.unwrap().to_string(),
+            self.last_key.unwrap().to_string()
+        ));
+    }
+}
+
+// Returns keys_offset, file_size, smallest key, biggest key.
+pub fn flush_to_disk<'a>(dir: &str, table_number: u64, m: &'a MemStore) -> Result<(u64, u64, String, String)> {
+    assert!(!m.entries.is_empty());
+    let mut builder = TableBuilder::<'a>::new();
+    
+    for (key, value) in m.entries.iter() {
+        builder.add_mutation(key, value);
+    }
     let mut f = std::fs::File::create(format!("{}/{}.tab", dir, table_number))?;
-    f.write_all(&value_buf)?;
-    f.write_all(&key_buf)?;
-    f.flush()?;
-    // NOTE: Fsync must happen at some point.
-    return Ok((
-        keys_offset,
-        keys_offset + key_buf.len() as u64,
-        first_key.unwrap().to_string(),
-        last_key.unwrap().to_string()));
+    return builder.finish(&mut f);
 }
 
 pub fn iterate_table(dir: &str, table_number: u64, func: &mut FnMut(String, Mutation) -> ()) -> Result<()> {
